6228e3aa0a650ed56c493131a6caf4c7df32620b
Claudiu <claudiu.dascalu@stud.ubbcluj.ro>
2021-10-07 17:42:39 +0300

    Mai e de lucrat

--- main.py
+++ main.py
@@ -1,53 +1,61 @@
-def get_longest_sorted_asc(lst: list[int]):
+from typing import List
+
+def show_menu():
+    print('1. Citire Date. ')
+    print('2. DeterminareDeterminare cea mai lunga secventa cu proprietatea ca numerele sunt ordonate crecator. ')
+    print(
+        '3. Determinare cea mai lunga secventa cu proprietatea ca toate numerele se pot scrie ca x**k, k citit, x întreg pozitiv. ')
+    print('4. Iesire. ')
+
+
+def read_list() -> List[int]:
+    lista = []
+    lista_str = input('Dati numerele separate prin spatiu: ')
+    lista_str_split = lista_str.split(' ')
+    for num_str in lista_str_split:
+        lista.append(int(num_str))
+    return lista
+
+
+def get_longest_sorted_asc(lst: List[int]) -> List[int]:
     '''
     Determinare cea mai lunga secventa cu proprietatea ca numerele sunt ordonate crescator.
     :param lst: O lista in care se va gasi cea mai lunga secventa cu proprietatea respectiva.
     :return: Cea mai lunga lista cu acea proprietate.
     '''
-    lista_definitiva = []
-    lista_temporara = []
-    lista_definitiva.append(lst[0])
-    contor = 1
-    crescator = 1
-    while crescator == 1:
-        if lst[contor] >= lst[contor - 1]:
-            lista_definitiva.append(lst[contor])
-            contor += 1
-        else:
-            crescator = 0
-    contor += 1
-    while contor != len(lst):
-        crescator = 1
-        while crescator == 1:
-            if lst[contor] >= lst[contor - 1]:
-                lista_temporara.append(lst[contor])
-                contor += 1
-            else:
-                crescator = 0
-            if contor == len(lst):
-                crescator = 0
-        if len(lista_temporara) > len(lista_definitiva):
-            lista_definitiva = lista_temporara
-    return lista_definitiva
 
-
-def test_get_longest_sorted_asc():
-    lst = [1, 2, 3, 2, 3, 4, 5, 3, 4, 5, 6, 7]
-    assert get_longest_sorted_asc(lst) == [3, 4, 5, 6, 7]
+    lungime = len(lst)
+    result = []
+    for st in range(lungime):
+        for dr in range(st, lungime):
+            sorted_asc = True
+            for num in lst[st:dr+1]:
+                if lst[num] < lst[num - 1]:
+                    sorted_asc = False
+                    break
+                if sorted_asc:
+                    if (dr - st + 1) > len(result):
+                        result = lst[st:dr + 1]
+    return result
 
 
 def main():
-    lista_str = input('1. Citire date. Introduceti un sir de numere separate prin spatiu: ')
-    lista_str_split = lista_str.split(' ')
-    lista_int = []
-    for lst_str in lista_str_split:
-        lista_int.append(int(lst_str))
-    print('2. Determinare cea mai lunga secventa cu proprietatea ca numerele sunt ordonate crecator: ')
-    print(get_longest_sorted_asc(lista_int))
-    print(
-        '3. Determinare cea mai lunga secventa cu proprietatea ca toate numerele se pot scrie ca x**k, k citit, x întreg pozitiv')
-    print('4. Iesire')
+    lista = []
+    while True:
+        show_menu()
+        optiunea = input('Alegeti optiunea: ')
+        if optiunea == '1':
+            lista = read_list()
+        elif optiunea == '2':
+            cresc = get_longest_sorted_asc(lista)
+            print('Lista cu cele mai multe numere in ordine crescatoare este: ', cresc)
+        elif optiunea == '3':
+            pass
+        elif optiunea == '4':
+            break
+        else:
+            print('Optiune invalida! Incercati alta optiune! ')
 
 
-test_get_longest_sorted_asc()
-main()
+if __name__ == '__main__':
+    main()
